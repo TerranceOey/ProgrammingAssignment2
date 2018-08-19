09737b690eccf9403d53e89542fe7ed8b3e02a02
TerranceOey <42516979+TerranceOey@users.noreply.github.com>
2018-08-19 11:19:50 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Clearing the previous R enviornment
+rm(list=ls())
 
-## Write a short comment describing this function
+## This file will contain two functions: makeCacheMatrix and cacheSolve
+## that will cache the inverse of a matrix fed into makeCacheMatrix.
+
+## The makeCacheMatrix is the first function, which creates a special
+## matrix object that will cache its inverse for a user inputted matrix.
+## It is assumed that the matrix is an invertible square matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() 
+    x
+  
+  setinv <- function(inverse) 
+    m <<- inverse
+  
+  getinv <- function() 
+    m
+  
+  list(set = set, 
+       get = get, 
+       setinv = setinv, 
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## The cacheSolve function is the second function which computes the inverse
+## of the matrix that was inputted into the makeCacheMatrix function above.
+## If the invertible square matrix has already been calculated, then the
+## cacheSolve function will output that. If the inverse of the matrix has not
+## been calculated, then the cacheSolve function will calculate the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  
+  if(!is.null(m)) {
+    message("getting cached result")
+    return(m)
+  }
+  
+  data <- x$get()
+  
+  m <- solve(data, ...)
+  
+  x$setinv(m)
+  
+  m
 }
+
+## Testing the makeCacheMatric and cacheSolve functions written above.
+test_matrix <- matrix(rnorm(9, 0, 1), 3, 3)
+test_matrix
+
+test_matrix.1 <- makeCacheMatrix(test_matrix)
+
+cacheSolve(test_matrix.1)
+
+
